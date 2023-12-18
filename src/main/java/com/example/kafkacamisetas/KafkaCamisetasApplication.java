787206e2abc2652267f79adb9a8db71d9b495a98
Nicolas Salazar <josalaza@bancolombia.com.co>
2023-12-17 23:53:00 -0500

    v1

---
+++ src/main/java/com/example/kafkacamisetas/KafkaCamisetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkacamisetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCamisetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaCamisetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkacamisetas/KafkaCamisetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkacamisetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCamisetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
